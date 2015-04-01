88c3ed89a4df4f9b67b9c8a9d900ac7094fba849
Garret Alfert <alfert@wevelop.de>
2015-04-01 19:34:43 +0200

    Always run specs in same order
    Needed to deactivate purdytest because it's
    incompatible with the required minitest version.
    Every minitest version after 5.3.3 doesn't 
    respect the specified sort order.

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ source "https://rubygems.org"
 gem "rest-client"
 
 gem 'rake'
-gem 'purdytest', :require => false
+# gem 'purdytest', :require => false
 gem 'm'
+gem 'minitest', '= 5.3.3'
 gem 'minitest-reporters'
--- spec_helper.rb
+++ spec_helper.rb
@@ -4,10 +4,17 @@ Bundler.require
 
 require 'minitest/spec'
 require 'minitest/autorun'
-require 'purdytest'
+# require 'purdytest'
 
 require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
+
+# Always run specs in the same order
+class Minitest::Spec
+  def self.test_order
+    :sorted
+  end
+end
 
 CONFIG = {
   host: 'http://storage.5apps.dev',
