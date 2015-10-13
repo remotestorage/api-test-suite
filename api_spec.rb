33d5815366121f13f7009abbaf49aa8d158e32a3
Greg Karékinian <greg@karekinian.com>
2015-10-13 21:32:26 +0200

    Add specs for public folder

--- api_spec.rb
+++ api_spec.rb
@@ -476,6 +476,92 @@ describe "Requests" do
         res.code.must_equal 401
       end
     end
+
+  end
+
+  describe "in a public folder" do
+
+    describe "PUT with a read/write category token" do
+      it "works" do
+        res = do_put_request("public/#{CONFIG[:category]}/test-object-simple.json",
+                             '{"new": "object"}',
+                             { content_type: "application/json" })
+
+        [200, 201].must_include res.code
+      end
+    end
+
+    describe "PUT with a read/write category token to wrong category" do
+      it "fails" do
+        res = do_put_request("public/othercategory/test-object-simple.json",
+                             '{"new": "object"}',
+                             { content_type: "application/json" })
+
+        [401, 403].must_include res.code
+      end
+    end
+
+    describe "GET without a token" do
+      it "works" do
+        res = do_get_request("public/#{CONFIG[:category]}/test-object-simple.json",
+                             authorization: nil)
+
+        res.code.must_equal 200
+      end
+    end
+
+    describe "HEAD without a token" do
+      it "works" do
+        res = do_head_request("public/#{CONFIG[:category]}/test-object-simple.json",
+                              authorization: nil)
+
+        res.code.must_equal 200
+      end
+    end
+
+    describe "PUT without a token" do
+      it "is not allowed" do
+        res = do_put_request("public/#{CONFIG[:category]}/test-object-simple-test.json",
+                             '{"new": "object"}',
+                             { content_type: "application/json",
+                               authorization: nil })
+
+        [401, 403].must_include res.code
+      end
+    end
+
+    describe "GET directory listing without a token" do
+      it "is not allowed" do
+        res = do_get_request("public/#{CONFIG[:category]}/", authorization: nil)
+
+        [401, 403].must_include res.code
+      end
+    end
+
+    describe "GET directory listing with a read-write category token" do
+      it "works" do
+        res = do_get_request("public/#{CONFIG[:category]}/")
+
+        res.code.must_equal 200
+      end
+    end
+
+    describe "DELETE without a token" do
+      it "is not allowed" do
+        res = do_delete_request("public/#{CONFIG[:category]}/test-object-simple.json",
+                                authorization: nil)
+
+        [401, 403].must_include res.code
+      end
+    end
+
+    describe "DELETE with a read/write category token" do
+      it "works" do
+        res = do_delete_request("public/#{CONFIG[:category]}/test-object-simple.json")
+
+        res.code.must_equal 200
+      end
+    end
   end
 
   describe "DELETE objects" do
